87c4760c6f5960c3597e476567a7dcbeff2d57ce
Dmitry <dimasiksosnin@mail.ru>
2023-03-24 21:10:10 +0500

    some tries

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ import json
 import os
 import time
 import wave
-
-
+from pydub import AudioSegment
+ffmpeg_path = 'ffmpeg.exe'
+# загрузка файла MP3
+# sound = AudioSegment.from_mp3(r"C:\Users\Dmitry\Downloads\X2Download.app - Финал Четырех Кубка России по баскетболу (192 kbps).mp3")
+# AudioSegment.converter = ffmpeg_path
+# # сохранение файла WAV
+# sound.export("test.wav", format="wav")
 #  https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip
-model = Model(r"C:\Users\CourtUser\Desktop\release\VoskGUI\vosk-model-small-ru-0.22")
+model = Model(r"vosk-model-small-ru-0.22")
 
 wf = wave.open(r'test.wav', "rb")
-rec = KaldiRecognizer(model, 16000)
+sample_rate = wf.getframerate()
+print(sample_rate)
+rec = KaldiRecognizer(model, sample_rate)
 
 result = ''
 last_n = False
-
+print('start recognition')
 while True:
-    data = wf.readframes(16000)
+    data = wf.readframes(sample_rate)
     if len(data) == 0:
         break
 
