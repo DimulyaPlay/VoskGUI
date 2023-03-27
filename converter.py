3da5bf7c8f39bb018c9ad343a420ae89c3c49577
DimulyaPlayHome <dimasiksosnin@mail.ru>
2023-03-27 08:22:02 +0500

    deleted queue that chatGPT misswrite

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QFileDialog, QVBoxLayout, QComboBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QFileDialog, QVBoxLayout, QComboBox, QMainWindow, QDialog
+from PyQt5 import uic
 from PyQt5.QtGui import QIcon
 from vosk import Model, KaldiRecognizer, SetLogLevel
 import os
@@ -7,6 +8,7 @@ import json
 import threading
 import subprocess
 import pyaudio
+from micselection import *
 #  pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/dimas/VoskGUI/icon.png" --add-data "C:\Python38\Lib\site-packages\vosk;vosk" --add-data "C:/Users/dimas/VoskGUI/ffmpeg.exe;." --add-data "C:/Users/dimas/VoskGUI/ffprobe.exe;." --add-data "C:/Users/dimas/VoskGUI/icon.png;." --add-data "C:/Users/dimas/VoskGUI/vosk-model-small-ru-0.22;vosk-model-small-ru-0.22/"  "C:/Users/dimas/VoskGUI/converter.py"
 
 
@@ -22,15 +24,19 @@ def resource_path(relative_path):
 
 
 class WorkerFile(threading.Thread):
-    def __init__(self, filename, model, text_edit, sr):
+    def __init__(self, filename, model, text_edit, sr, b1, b2):
         super(WorkerFile, self).__init__()
         self.filename = filename
         self.model = model
         self.text_edit = text_edit
         self.sr = sr
         self.stop_event = threading.Event()
+        self.b1 = b1
+        self.b2 = b2
 
     def run(self):
+        self.b1.setDisabled(True)
+        self.b2.setDisabled(True)
         if self.filename.endswith(".mp3"):
             new_filename = os.path.splitext(self.filename)[0] + ".wav"
             subprocess.call(['ffmpeg', '-y', '-i', self.filename, new_filename], creationflags=subprocess.CREATE_NO_WINDOW)
@@ -59,97 +65,109 @@ class WorkerFile(threading.Thread):
 
     def stop(self):
         self.stop_event.set()
-        process_button.setDisabled(False)
-        rec_button.setDisabled(False)
+        self.b1.setDisabled(False)
+        self.b2.setDisabled(False)
 
 
 class WorkerLive(threading.Thread):
-    def __init__(self, model, text_edit):
+    def __init__(self, model, text_edit, b1, b2, mic_chosen):
         super(WorkerLive, self).__init__()
         self.model = model
         self.text_edit = text_edit
         self.stop_event = threading.Event()
-        self.rec = KaldiRecognizer(self.model, 16000)
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2000)
+        self.mic_list = mic_chosen
+        print('running1')
+        if not self.mic_list:
+            self.stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=16000, input=True,
+                                                 frames_per_buffer=2000)
+            self.rec = KaldiRecognizer(self.model, 16000)
+        else:
+            self.stream = [pyaudio.PyAudio().open(
+                format=pyaudio.paInt16,
+                channels=1,
+                rate=16000,
+                input=True,
+                input_device_index=index,
+                frames_per_buffer=2000) for index in self.mic_list]
+            self.rec = [KaldiRecognizer(self.model, 16000) for _ in range(len(self.mic_list))]
+        self.b1 = b1
+        self.b2 = b2
+        print('running2')
 
     def run(self):
+        print('running3')
+        # self.b1.setDisabled(True)
+        # self.b2.setDisabled(True)
         self.text_edit.append('Распознавание с микрофона началось\n\n')
         while True:
             if self.stop_event.is_set():
                 self.text_edit.append('\n\nРаспознавание было остановлено пользователем.')
                 return
-            data = self.stream.read(2000)
-            if len(data) == 0:
-                break
-            if self.rec.AcceptWaveform(data):
-                result = json.loads(self.rec.Result())
-                text = result['text']
-                self.text_edit.append(text)
+            if not self.mic_list:
+                data = self.stream.read(2000)
+                if self.rec.AcceptWaveform(data):
+                    result = json.loads(self.rec.Result())
+                    text = result['text']
+                    self.text_edit.append(text)
+            else:
+                data = [device.read(2000) for device in self.stream]
+                for recognizer, audio_data in zip(self.recognizers, data):
+                    if recognizer.AcceptWaveform(audio_data):
+                        result = json.loads(recognizer.Result())
+                        self.text_edit.append(text)
 
     def stop(self):
         self.stop_event.set()
-        process_button.setDisabled(False)
-        rec_button.setDisabled(False)
+        # self.b1.setDisabled(False)
+        # self.b2.setDisabled(False)
 
 
-def process_file():
-    file_dialog = QFileDialog()
-    filename, _ = file_dialog.getOpenFileName(None, "Выберите аудиофайл", "", "Audio Files (*.wav *.mp3)")
-    if not filename:
-        return
-    model = Model(resource_path("vosk-model-small-ru-0.22"))
-    sr = combo_box.currentText()
-    worker_thread = WorkerFile(filename, model, text_edit, sr)
-    stop_button.clicked.connect(worker_thread.stop)
-    worker_thread.start()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(resource_path('main_window.ui'), self)
+        self.setWindowTitle("Распознавание речи. Vosk RUS recognition GUI by Dmitry Sosnin")
+        self.setWindowIcon(QIcon(resource_path('icon.png')))
+        self.text_edit = self.findChild(QTextEdit, 'textEdit')
+        self.combo_box = self.findChild(QComboBox, 'comboBox')
+        self.combo_box.addItems(['4000', '8000', '16000', 'Исходный Samplerate'])
+        self.process_button = self.findChild(QPushButton, 'pushButton_fromFile')
+        self.process_button.clicked.connect(self.process_file)
+        self.rec_button = self.findChild(QPushButton, 'pushButton_fromMic')
+        self.rec_button.clicked.connect(self.process_live)
+        self.stop_button = self.findChild(QPushButton, 'pushButton_stop')
+        self.choose_mics = self.findChild(QPushButton, 'pushButton_chooseMic')
+        self.choose_mics.clicked.connect(self.choose_mics_window)
+        clear_button = self.findChild(QPushButton, 'pushButton_clear')
+        clear_button.clicked.connect(lambda: self.text_edit.clear())
+        self.model = Model(resource_path("vosk-model-small-ru-0.22"))
+        self.mic_chosen = []
+        self.show()
 
+    def process_file(self):
+        print(self.mic_chosen)
+        file_dialog = QFileDialog(self)
+        filename, _ = file_dialog.getOpenFileName(None, "Выберите аудиофайл", "", "Audio Files (*.wav *.mp3)")
+        if not filename:
+            return
+        sr = self.combo_box.currentText()
+        worker_thread = WorkerFile(filename, self.model, self.text_edit, sr, self.process_button, self.rec_button)
+        self.stop_button.clicked.connect(worker_thread.stop)
+        worker_thread.start()
 
-def process_live():
-    process_button.setDisabled(True)
-    rec_button.setDisabled(True)
-    model = Model(resource_path("vosk-model-small-ru-0.22"))
-    worker_thread = WorkerLive(model, text_edit)
-    stop_button.clicked.connect(worker_thread.stop)
-    worker_thread.start()
+    def process_live(self):
+        worker_thread = WorkerLive(self.model, self.text_edit, self.process_button, self.rec_button, self.mic_chosen)
+        stop_button.clicked.connect(worker_thread.stop)
+        worker_thread.start()
 
+    def choose_mics_window(self):
+        mw = MicrophoneSelectionWindow(self)
 
-def closeEvent(self, event):
-    QApplication.quit()
-    event.accept()
+    def closeEvent(self, event):
+        QApplication.quit()
+        event.accept()
 
 
 app = QApplication([])
-window = QWidget()
-window.setWindowTitle("Распознавание речи. Vosk RUS recognition GUI by Dmitry Sosnin")
-window.setWindowIcon(QIcon(resource_path('icon.png')))
-window.closeEvent = closeEvent
-
-text_edit = QTextEdit()
-text_edit.setReadOnly(True)
-
-combo_box = QComboBox()
-combo_box.addItems(['4000', '8000', '16000', 'Исходный Samplerate'])
-
-process_button = QPushButton("Обработать файл")
-process_button.clicked.connect(process_file)
-
-rec_button = QPushButton("С микрофона")
-rec_button.clicked.connect(process_live)
-
-stop_button = QPushButton("Остановить")
-
-clear_button = QPushButton('Очистить вывод')
-clear_button.clicked.connect(lambda: text_edit.clear())
-
-layout = QVBoxLayout()
-layout.addWidget(text_edit)
-layout.addWidget(combo_box)
-layout.addWidget(process_button)
-layout.addWidget(rec_button)
-layout.addWidget(stop_button)
-layout.addWidget(clear_button)
-
-window.setLayout(layout)
-window.show()
+window = MainWindow()
 app.exec_()
---
+++ micselection.py
@@ -0,0 +1,61 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QCheckBox, QPushButton, QVBoxLayout, QDialog
+from PyQt5.QtCore import Qt
+import pyaudio
+
+
+class MicrophoneSelectionWindow(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.parent = parent
+        # Получение списка микрофонов с их индексами
+        self.mic_dict = {}
+        self.p = pyaudio.PyAudio()
+        for i in range(self.p.get_device_count()):
+            dev = self.p.get_device_info_by_index(i)
+            if dev['maxInputChannels'] > 0:
+                self.mic_dict[dev['index']] = dev['name'].encode('cp1251').decode('utf-8')
+        self.initUI()
+        self.show()
+
+    def initUI(self):
+        # Заголовок окна
+        self.setWindowTitle("Выберите микрофоны")
+
+        # Описание окна
+        self.description = QLabel("Выберите микрофон(ы), которые хотите использовать:")
+        self.description.setAlignment(Qt.AlignTop)
+
+        # Чекбоксы для выбора микрофонов
+        self.mic_checkboxes = []
+        for index, name in self.mic_dict.items():
+            checkbox = QCheckBox(name)
+            checkbox.index = index  # сохраним индекс микрофона в самом виджете
+            checkbox.setChecked(index in self.parent.mic_chosen)
+            self.mic_checkboxes.append(checkbox)
+
+        # Кнопка применения настроек
+        self.apply_button = QPushButton("Применить")
+        self.apply_button.clicked.connect(self.apply_settings)
+
+        # Размещение виджетов на окне
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.description)
+        for checkbox in self.mic_checkboxes:
+            vbox.addWidget(checkbox)
+        vbox.addWidget(self.apply_button)
+
+        self.setLayout(vbox)
+
+    def apply_settings(self):
+        # Сбор выбранных микрофонов и их индексов
+        selected_mics = []
+        for checkbox in self.mic_checkboxes:
+            if checkbox.isChecked():
+                selected_mics.append(checkbox.index)
+        self.parent.mic_chosen = selected_mics
+        # Закрытие окна и возврат списка индексов микрофонов
+        self.close()
+
+
+
+
